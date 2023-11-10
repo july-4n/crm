079ed17ebd537fc571b027317bcb506b01db3802
July-4n <september4n@gmail.com>
2023-11-10 22:56:31 +0500

    solve mod2 task2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const modalTitle = modal.querySelector('.modal__title');
+const modalForm = modal.querySelector('.modal__form');
+const modalInputCheckbox = modal.querySelector('.modal__checkbox');
+const modalInputDiscount = modal.querySelector('.modal__input_discount');
